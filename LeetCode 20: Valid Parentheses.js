1ec6811df8e03f14c982d8f5aa0ea5d14de04424
Nimra Ahmed <50912134+nimraahmed@users.noreply.github.com>
2023-07-21 18:52:39 +0500

    Update LeetCode 20: Valid Parentheses.js

--- LeetCode 20: Valid Parentheses.js
+++ LeetCode 20: Valid Parentheses.js
@@ -1,27 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 var isValid = function(s) {
     let parentheses = {
         '(': ')',
-        '{' : '}',
+        '{': '}',
         '[': ']'
-    }
-    if(s.length%2===1){
-        return false
-    }
-    if(s[0]=== ')'|| s[0]==='}'|| s[0]===']'){
-        return false
-    }
-    if(s[s.length-1]==='(' || s[s.length-1]==='{'||s[s.length-1]==='['){
-        return false
+    };
+
+    if (s.length % 2 !== 0) {
+        return false; 
     }
 
-    let stack = []
-    for(let i=0; i<s.length;i++){
-        if(s[i]==='['||s[i]==='{'||s[i]==='('){
-            stack.push(s[i])
-        } else if(parentheses[stack.pop()]!==s[i]){
-            return false 
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '[' || s[i] === '{' || s[i] === '(') {
+            stack.push(s[i]);
+        } else {
+            let lastOpening = stack.pop();
+            if (parentheses[lastOpening] !== s[i]) {
+                return false; 
+            }
         }
     }
-    return stack.length===0
 
+    return stack.length === 0;
 };
