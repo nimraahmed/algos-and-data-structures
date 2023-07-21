84d098c7a1e560ba0d26c9a4c256ef25fc90e48f
Nimra Ahmed <50912134+nimraahmed@users.noreply.github.com>
2023-07-21 18:55:58 +0500

    Update LeetCode 20: Valid Parentheses.js

--- LeetCode 20: Valid Parentheses.js
+++ LeetCode 20: Valid Parentheses.js
@@ -27,3 +27,8 @@ var isValid = function(s) {
 
     return stack.length === 0;
 };
+
+console.log(isValid('(]')) //false
+console.log(isValid('(([]){})')) //true
+console.log(isValid('{[()]}')) //true
+console.log(isValid('(((')) //false
